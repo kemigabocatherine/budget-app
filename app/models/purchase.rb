86cf9b5a89cc5e20a8405d5cb6703000514eadab
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-08-06 16:37:08 +0300

    Fix all linter errors

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -5,13 +5,4 @@ class Purchase < ApplicationRecord
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
-
-  after_save :amount_updater
-
-  private
-
-  def amount_updater
-    total_transactions = 0
-    total_transactions += purchase.amount
-  end
 end
